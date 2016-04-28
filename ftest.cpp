c9c1ba2ceeb3c137d92d981aa114be1e47277e6c
Bill-Gray <pluto@projectpluto.com>
2016-04-28 09:40:26 -0400

    Ensured that __STDC_LIMIT_MACROS is always defined.  That got rid of a problem with older g++ in which UINT64_MAX was left undefined.  Also switched from DOS/Windows-style CR/LF line endings to LF-only.

--- ftest.cpp
+++ ftest.cpp
@@ -17,6 +17,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301, USA.    */
 
+#ifndef __STDC_LIMIT_MACROS
+   #define __STDC_LIMIT_MACROS
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
